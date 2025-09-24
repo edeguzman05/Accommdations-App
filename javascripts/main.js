d7f449d05bfd2eb99cfec711072d863bfef61a57
edeguzman05 <edeguzmanjr05@csu.fullerton.edu>
2025-09-24 07:20:07 -0700

    UI is fully operational! Prototype appears to be cbecoming closer to working. Need to add a few more changes but it's good enough!

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,18 @@
-function loadTab(tabId) {
+function loadTab(event, tabId) {
     // Hide all sections
     document.querySelectorAll('.tab-content').forEach(section => {
         section.classList.remove('active');
     });
 
     // Show the one that matches the button pressed
-    document.getElementById(tabId).classList.add('active');
+    const target = document.getElementById(tabId);
+    if (target) {
+        target.classList.add('active');
+    }
 
     document.querySelectorAll('.sidebar button').forEach(btn => {
         btn.classList.remove('active');
     });
 
-    document.target.classList.add('active');
+    event.currentTarget.classList.add('active');
 }
